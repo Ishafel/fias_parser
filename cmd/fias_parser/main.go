f84a69ebd427d608a4836a13e2ffa8101408c14d
Artem <artem.zavalishin@yandex.ru>
2025-12-13 11:31:05 +0300

    Add directory XML parsing and remove test data

--- cmd/fias_parser/main.go
+++ cmd/fias_parser/main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 
 	"fias_parser/pkg/schema"
 	"fias_parser/pkg/xmlstream"
@@ -11,11 +14,11 @@ import (
 
 func main() {
 	schemaDir := flag.String("schema-dir", "gar_schemas", "Directory with GAR XSD schemas")
-	xmlFile := flag.String("xml", "", "Path to XML file to parse")
+	xmlPath := flag.String("xml", "", "Path to XML file or directory to parse")
 	elementName := flag.String("element", "", "Name of the element to stream (defaults to first child of root)")
 	flag.Parse()
 
-	if *xmlFile == "" {
+	if *xmlPath == "" {
 		fmt.Fprintln(os.Stderr, "--xml is required")
 		os.Exit(1)
 	}
@@ -26,22 +29,64 @@ func main() {
 		os.Exit(1)
 	}
 
-	xmlRoot, err := xmlstream.DetectXMLRoot(*xmlFile)
+	files, err := collectXMLFiles(*xmlPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "detect xml root: %v\n", err)
+		fmt.Fprintf(os.Stderr, "collect xml files: %v\n", err)
 		os.Exit(1)
 	}
 
-	currentSchema, ok := schemas[xmlRoot]
-	if !ok {
-		fmt.Fprintf(os.Stderr, "no schema found for root element %q\n", xmlRoot)
-		os.Exit(1)
+	for _, file := range files {
+		xmlRoot, err := xmlstream.DetectXMLRoot(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "detect xml root in %s: %v\n", file, err)
+			os.Exit(1)
+		}
+
+		currentSchema, ok := schemas[xmlRoot]
+		if !ok {
+			fmt.Fprintf(os.Stderr, "no schema found for root element %q in %s\n", xmlRoot, file)
+			os.Exit(1)
+		}
+
+		fmt.Fprintf(os.Stderr, "Using schema %s for root element %s in %s\n", currentSchema.Path, currentSchema.RootElement, file)
+
+		if err := xmlstream.StreamElements(file, *elementName, os.Stdout); err != nil {
+			fmt.Fprintf(os.Stderr, "stream xml from %s: %v\n", file, err)
+			os.Exit(1)
+		}
+	}
+}
+
+func collectXMLFiles(path string) ([]string, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return nil, err
 	}
 
-	fmt.Printf("Using schema %s for root element %s\n", currentSchema.Path, currentSchema.RootElement)
-
-	if err := xmlstream.StreamElements(*xmlFile, *elementName, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "stream xml: %v\n", err)
-		os.Exit(1)
+	if !info.IsDir() {
+		return []string{path}, nil
 	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		name := entry.Name()
+		if strings.EqualFold(filepath.Ext(name), ".xml") {
+			files = append(files, filepath.Join(path, name))
+		}
+	}
+
+	sort.Strings(files)
+	if len(files) == 0 {
+		return nil, fmt.Errorf("no xml files found in %s", path)
+	}
+
+	return files, nil
 }
